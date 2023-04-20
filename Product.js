2c953aa2c9f11ec870ee92371c6804c8c3a4f0e7
Karina <vbank322@mail.ru>
2023-04-21 00:09:26 +0300

    +

--- Product.js
+++ Product.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const Product = new mongoose.Schema({
 
-    _id: {type: Number, required: false},
+    _id: {required: false},
     subtitle: {type: String, required: true},
     type: {type: String, required: true}
 })
--- index.js
+++ index.js
@@ -10,7 +10,8 @@ app.use('/api', router)
 
 async function startApp() {
     try {
-        await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+        await mongoose.connect(process.env.DB_URL)
+        // await mongoose.connect(process.env.DB_URL, {useNewUrlParser: true, useUnifiedTopology: true})
         app.listen(process.env.PORT || 4444, () => console.log('SERVER STARTED ON PORT' + process.env.PORT || 4444))
     } catch (e) {
         console.log(e)
