5d4098c1d358b05fc59da82a33309c253c03a8cc
Karina <vbank322@mail.ru>
2023-04-20 23:43:19 +0300

    +

--- Product.js
+++ Product.js
@@ -4,7 +4,7 @@ const Product = new mongoose.Schema({
 
     _id: {type: Number, required: false},
     subtitle: {type: String, required: true},
-    type: {type: String, required: true},
+    type: {type: String, required: true}
 })
 
 export default mongoose.model('Product', Product)
