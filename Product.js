f3a47f82fce680857d98f8925f4fae8f0c4de302
Karina <vbank322@mail.ru>
2023-04-21 00:21:22 +0300

    +

--- Product.js
+++ Product.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const Product = new mongoose.Schema({
 
-
+    _id: {required: false},
     subtitle: {type: String, required: true},
     type: {type: String, required: true}
 })
